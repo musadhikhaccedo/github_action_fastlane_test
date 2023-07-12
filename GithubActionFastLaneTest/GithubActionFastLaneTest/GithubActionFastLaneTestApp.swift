9346aaac30e589f811bcdfdce3a3581b750bb8ea
musadhikh muhammed <musadhikh.muhammed@accedo.tv>
2023-07-12 11:43:59 +0800

    [NO-TICKET] Project created

---
+++ GithubActionFastLaneTest/GithubActionFastLaneTest/GithubActionFastLaneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionFastLaneTestApp.swift
+//  GithubActionFastLaneTest
+//
+//  Created by Musadhikh Muhammed K on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionFastLaneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
